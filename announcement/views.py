226fee1a9a98ee823602d268e2d057ed66497826
sherzod_a_thinkland <sherzod_a@thinkland.uz>
2023-12-24 22:42:03 +0500

    !@#@

--- announcement/views.py
+++ announcement/views.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -11,8 +11,12 @@ from announcement.serializers import TransportSerializer, AnnouncementSerializer
 class TransportViewSet(ModelViewSet):
     queryset = Transports.objects.all()
     serializer_class = TransportSerializer
-
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class AnnouncementViewSet(ModelViewSet):
     queryset = Announcement.objects.all()
     serializer_class = AnnouncementSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 def image_upload_path(instance, filename):
@@ -11,6 +12,7 @@ class Images(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     image = models.ImageField(upload_to=image_upload_path)
     name = models.CharField(max_length=255, null=True, blank=True)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def __str__(self):
         return f"{self.uuid}/{self.name}"
