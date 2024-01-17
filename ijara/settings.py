cefef3ae597f39e7b2d282fce95fdfeebef44be3
Abdumutalov <sh.abdumutalov1@gmail.com>
2024-01-17 17:51:50 +0500

    1111

--- ijara/settings.py
+++ ijara/settings.py
@@ -15,8 +15,6 @@ from datetime import timedelta
 import os.path
 import sys
 
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -162,10 +160,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
