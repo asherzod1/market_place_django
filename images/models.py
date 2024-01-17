d685258e0320fcf92c6325302e078289011f4c3e
Abdumutalov <sh.abdumutalov1@gmail.com>
2024-01-17 15:39:01 +0500

    1111

--- images/models.py
+++ images/models.py
@@ -1,7 +1,6 @@
 import uuid
 
 from django.db import models
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 def image_upload_path(instance, filename):
@@ -12,7 +11,7 @@ class Images(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     image = models.ImageField(upload_to=image_upload_path)
     name = models.CharField(max_length=255, null=True, blank=True)
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = []
 
     def __str__(self):
         return f"{self.uuid}/{self.name}"
