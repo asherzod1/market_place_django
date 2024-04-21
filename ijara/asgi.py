070b93995a139e458ce1145f198c9d2fa8b1c76e
Asherzod1 <sh.abdumutalov1@gmail.com>
2024-04-21 08:47:20 -0500

    Web socket added to comments finished

--- ijara/asgi.py
+++ ijara/asgi.py
@@ -13,14 +13,10 @@ import os
 from .middlewares import JwtAuthMiddlewareStack
 from .socket_urls import websocket_urlpatterns
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ijara.settings')
-
-django.setup()
 
 django_asgi_app = get_asgi_application()
 
--- ijara/middlewares.py
+++ ijara/middlewares.py
@@ -4,7 +4,7 @@ from channels.middleware import BaseMiddleware
 from channels.auth import AuthMiddlewareStack
 from urllib.parse import parse_qs
 
-from django.conf import settings
+from .settings import SECRET_KEY
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
 
@@ -21,7 +21,7 @@ class JwtAuthMiddleware(BaseMiddleware):
         token = query_string.get('token', [None])[0]
         if token:
             try:
-                decoded_data = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+                decoded_data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
                 user = await self.get_user(decoded_data['user_id'])
                 scope['user'] = user if user else AnonymousUser()
             except jwt.ExpiredSignatureError:
