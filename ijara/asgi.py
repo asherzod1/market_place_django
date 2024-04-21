b11f3cf0ced5b8f51df96b7541f0edfec286400f
Asherzod1 <sh.abdumutalov1@gmail.com>
2024-04-21 09:24:18 -0500

    Web socket added to comments finished

--- ijara/asgi.py
+++ ijara/asgi.py
@@ -16,14 +16,11 @@ from .socket_urls import websocket_urlpatterns
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ijara.settings')
-django.setup()
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": JwtAuthMiddlewareStack(
           URLRouter(
                 websocket_urlpatterns
